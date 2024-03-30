80d2b306af260371027edf4a0d79c1d60b338a45
juanpo12 <juanjoc2000@hotmail.com>
2024-03-30 01:12:43 -0300

    refactor searchbar

--- src/components/navbar/Searchbar.jsx
+++ src/components/navbar/Searchbar.jsx
@@ -1,31 +1,15 @@
+import Input from "../ui/Input"
 
-const Searchbar = ({ setValue }) => {
+const Searchbar = ({ setFilters }) => {
 
     return(
         <div className='flex gap-2 items-center'>
-            <input
+
+            <Input
                 placeholder='Search characters'
-                onChange={(e) => setValue(e.target.value)}
-                onKeyUp={(e) => {
-                    if (e.key === 'Enter') {
-                        console.log('Searching...');
-                    }
-                }}
-                className={'rounded-full px-4 py-2 h-fit'}/>
-            <button
-                onClick={() => {
-                    console.log('Searching...');
-                }}
-                className='bg-transparent'
-            >
-                <img
-                    src='/buscar.png'
-                    alt='search'
-                    width={20}
-                    height={20}
-                    className='hidden sm:flex cursor-pointer items-center'
-                />
-            </button>
+                onChange={(e) => setFilters(e.target.value)}
+                className={'rounded-full px-4 py-2 h-fit'}
+            />
     </div>
     )
 }
