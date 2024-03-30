6401c19b34859bcc3822530c7c3210ed241205fc
juanpo12 <juanjoc2000@hotmail.com>
2024-03-30 01:11:37 -0300

    Feat[Cards]:Changed at CharacterList

---
+++ src/components/CharacterList.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react"
+import Card from "./Card"
+import Modal from "./Modal"
+
+const CharacterList = ({characters, loading, error}) => {
+    const [isOpen, setIsOpen] = useState(false)
+
+    const [selectedCharacter, setSelectedCharacter] = useState({})
+
+    const handleCardClick = (item) => {
+        setSelectedCharacter(item);
+        setIsOpen(true);
+    };
+    
+    console.log(selectedCharacter);
+    return(
+        <div className="sm:grid sm:grid-cols-3 md:grid-cols-4">
+            {
+                characters?.map((item) => {
+                    return (
+                        <div className="flex justify-center mt-10" key={item.name}>
+                                <Card
+                                    handleCardClick={handleCardClick}
+                                    image={item.image}
+                                    name={item.name}
+                                    species={item.species}
+                                    gender={item.gender}
+                                    status={item.status}
+                                />
+                        </div>
+                    )
+                })
+            }
+            {isOpen && (
+                <Modal setIsOpen={setIsOpen} isOpen={isOpen}>
+                    <div className="text-center">
+                        <img src={selectedCharacter.image} alt={selectedCharacter.name} className="mx-auto mb-4 rounded-full border-2 border-violet-800 shadow-2xl" />
+                        <h2 className="text-2xl font-bold mb-4">{selectedCharacter.name}</h2>
+                        <p className="text-lg">{selectedCharacter.species}</p>
+                        <p className="text-lg">{selectedCharacter.gender}</p>
+                        <p className="text-lg">{selectedCharacter.status}</p>
+                    </div>
+                </Modal>
+            )}
+            {
+                loading && <Modal>
+                    <div className="text-center">
+                        <h2 className="text-2xl font-bold mb-4">Loading...</h2>
+                    </div>
+                </Modal> 
+            }
+            
+    
+
+        </div>
+    )
+}
+
+export default CharacterList
