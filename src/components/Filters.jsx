5e4445f15f888f859644565176efb0a6b8913d8a
juanpo12 <juanjoc2000@hotmail.com>
2024-03-30 01:13:56 -0300

    feat[app]:Added filters and charecterList

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,14 +1,8 @@
-import { useState } from "react"
 import Select from "./ui/Select"
 import Button from "./ui/Button"
 
 
-const Filters = () => {
-    const [filters, setFilters] = useState({
-        status: '',
-        species: '',
-        gender: ''
-    })
+const Filters = ({filters, setFilters}) => {
 
     const gender = ['Male', 'Female', 'unknown']
     const status = ['Alive', 'Dead', 'unknown']
@@ -27,7 +21,7 @@ const Filters = () => {
     return (
         <div className="bg-violet-600/60 p-2 gap-4 md:rounded-xl flex flex-col md:m-10 shadow-lg shadow-violet-800 md:w-1/4 h-fit">
             <h1 className="text-2xl font-bold text-center">Filters</h1>
-            <div className="flex flex-col p-4 gap-2">
+            <div className="flex sm:flex-col p-4 gap-2">
                 <Select options={status} value={filters.status} placeholder="Status" setValue={(value) => setFilters({...filters, status: value})}/>
                 <Select options={species} value={filters.species} placeholder="Species" setValue={(value) => setFilters({...filters, species: value})}/>
                 <Select options={gender} value={filters.gender} placeholder="Gender" setValue={(value) => setFilters({...filters, gender: value})}/>
